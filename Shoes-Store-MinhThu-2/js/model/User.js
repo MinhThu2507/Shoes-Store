d35d7c80da8b6c14714be0c6a27a53cb6d508be2
MinhThu2507 <113906607+MinhThu2507@users.noreply.github.com>
2022-11-05 00:27:21 +0700

    Delete Shoes-Store-MinhThu-2 directory

--- Shoes-Store-MinhThu-2/js/model/User.js
+++
@@ -1,9 +0,0 @@
-class User{
-    constructor(email, password, name, phone, gender) {
-        this.email = email;
-        this.password = password;
-        this.name = name;
-        this.phone = phone;
-        this.gender = gender;
-    }
-}
